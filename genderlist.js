21b50c31cc9289a68ee03f4efa31fb424d92ca21
skylord-a52 <skylord-a52@users.noreply.github.com>
2018-11-02 01:14:46 -0700

    Update genderlist.js

--- genderlist.js
+++ genderlist.js
@@ -1,1 +1,13 @@
 
+    var gender = [
+      "The 2-vector [x, y], where x &#8712 (0, boy), y &#8712 (0, girl)",
+      "Mathematician",
+      "Nonbinary by elimination",
+      "Nonbinary by contradiction",
+      "A → ¬A",
+      "The excluded middle",
+      "Gender? I barely know 'er!",
+      "<confused/reluctant> <boy/girl> by day, <reluctant/confused> <girl/boy> by night",
+      "Bulbasaur"
+    ];
+    
