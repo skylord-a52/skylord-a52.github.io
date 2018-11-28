7d9a942b7b88fead1a15375d3cdbf58a7e851e86
skylord-a52 <skylord-a52@users.noreply.github.com>
2018-11-27 16:08:21 -0800

    Update genderlist.js

--- genderlist.js
+++ genderlist.js
@@ -1,14 +1,17 @@
 
-    var gender = [
-      "Some 2-vector [x, y], where x &#8712 (0, boy), y &#8712 (0, girl)",
-      "Mathematician",
-      "Nonbinary by elimination",
-      "Nonbinary by contradiction",
-      "A → ¬A",
-      "The excluded middle",
-      "Gender? I barely know 'er!",
-      "<confused/reluctant> <boy/girl> by day, <reluctant/confused> <girl/boy> by night",
-      "Bulbasaur",
-      "There is as yet insufficient data for a meaningful answer"
-    ];
-    
+var gender = [
+    "Some 2-vector [x, y], where x &#8712 (0, boy), y &#8712 (0, girl)",
+    "Mathematician",
+    "Nonbinary by elimination",
+    "Nonbinary by contradiction",
+    "A → ¬A",
+    "The excluded middle",
+    "Gender? I barely know 'er!",
+    "<confused/reluctant> <boy/girl> by day, <reluctant/confused> <girl/boy> by night",
+    "Bulbasaur",
+    "There is as yet insufficient data for a meaningful answer",
+    "Up to interpretation",
+    "Cryptid dumbass",
+    "<img src="question.gif style="width:50px;height:60px;"">,
+    "A goddamn mess"
+];
